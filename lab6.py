b980d87208fa974e49cb28391e589dfbfa362abf
TrueProtektor <72074939+TrueProtektor@users.noreply.github.com>
2023-04-25 18:38:47 +0500

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -15,12 +15,13 @@ option = st.selectbox(
 if option == 'задача 1)':
   user_input_class = st.text_input("Введите класс пасажира "1, 2 или 3" >>: ")
   user_input_sex = st.text_input("Пол пасажира "male или female>>: ")
-  
+  sik =df[(df['Pclass']==user_input_class) & (df['Sex']==user_input_sex) & (df['Survived']==1)][["Name","Sex","Age"]]
   st.dataframe(sik)
 if option == 'задача 2)':
   option_2 = st.selectbox(
     'Выберете',
     ('спасен', 'нет'))
+  df[(df["Fare"]==0.0) & (df['Survived']==searched_survived)]
   st.dataframe(spas)
 else: st.write(
     "#Выберете задачу"
